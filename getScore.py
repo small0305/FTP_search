f0b4e5774ba25973906c8a7cf83668011d45afe9
Ran Tao <04small@gmail.com>
2016-03-18 19:17:27 +0800

    Update getScore.py
    process several files in a document
    process data that are not saved in number but in string (which is not very successful)

--- getScore.py
+++ getScore.py
@@ -6,7 +6,23 @@ Created on Thu Mar 17 21:53:07 2016
 """
 import numpy as np
 import pandas as pd
-data = pd.read_excel('20151011-“阳光计划”体验式培训-团委邵宪一.xlsx')
-result = data[data['学号'] == 115032910018]
-results = pd.DataFrame(result,columns = ['学号','姓名','指标内容','分数'])
+import os
+
+root_dir = 'C:\\学习\\素拓\\党团'
+ID = 115032910018
+filelist = [x for x in os.listdir(root_dir) if os.path.splitext(x)[1]=='.xlsx']
+results = pd.DataFrame(columns = ['学号','姓名','指标内容','分数'])
+for name in filelist:
+    filename = os.path.join(root_dir,name)
+    data = pd.read_excel(filename)
+    if data['学号'].dtypes != 'int64':
+        
+#        data['学号'].astype(str)
+        data['学号'] = data['学号'].fillna(0)
+        data['学号'].astype(np.int64)
+        
+
+    result = data[data['学号'] == ID]
+    results = results.append(pd.DataFrame(result,columns = ['学号','姓名','指标内容','分数']))
+
 
